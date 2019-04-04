b7d815683a7ebcb41ff2e0665e47eea6de7638de
Ian Carlos <carlos.ian007@gmail.com>
2019-04-03 21:15:32 -0300

    Correcting Item Model

--- server/models/Item.js
+++ server/models/Item.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema();
+const Schema = mongoose.Schema;
 
-const ItemSchema = Schema({
+const ItemSchema = new Schema({
   name: {
     type: String,
     required: true
   },
   date: {
     type: Date,
-    default: Date.now()
+    default: Date.now
   }
 });
 

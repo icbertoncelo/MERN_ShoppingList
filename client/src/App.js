46930c465aa2b093f39615aed39cd4107056850a
Ian Carlos <carlos.ian007@gmail.com>
2019-04-16 23:14:27 -0300

    Adding Auth State

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,17 @@ import ShoppingList from "./components/ShoppingList";
 import ItemModal from "./components/ItemModal";
 import { Provider } from "react-redux";
 import store from "./store";
+import { loadUser } from "./actions/authActions";
 
 import { Container } from "reactstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 class App extends Component {
+  componentDidMount() {
+    store.dispatch(loadUser());
+  }
+
   render() {
     return (
       <Provider store={store}>
